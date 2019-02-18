9611440cc8bec60b85a28d7a9c0de56f50e433b8
jli2021 <34481811+jli2021@users.noreply.github.com>
2019-02-18 11:31:32 -0500

    Add files via upload

---
+++ background.js
@@ -0,0 +1,33 @@
+var urls = [
+"http://youtube.com/", 
+"http://google.com/", 
+"http://stackoverflow.com/",
+"http://yahoo.com/"
+];
+
+var startURL = "http://pingry.org/";
+
+function showURL(i){
+	chrome.tabs.update(null, {url: urls[i]});
+};
+
+
+var x = 0;                     
+
+function myLoop () { 
+   setTimeout(function () {    /
+      chrome.tabs.update(null, {url: urls[x%4]});       
+      x++;                     
+      if (x < urls.length * 3) {
+         myLoop();
+      }
+   }, 2000)
+}
+
+
+
+chrome.browserAction.onClicked.addListener(function(activeTab){
+	chrome.tabs.create({url: startURL});
+	myLoop();
+});
+
